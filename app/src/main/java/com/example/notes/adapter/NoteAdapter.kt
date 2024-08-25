ae2b8b7094f1df0e5a3cda8a1fb2fb5fbe327ea5
GGennady <lessarent@gmail.com>
2024-08-25 03:50:42 +0300

    Upgraded

--- app/src/main/java/com/example/notes/adapter/NoteAdapter.kt
+++ app/src/main/java/com/example/notes/adapter/NoteAdapter.kt
@@ -1,4 +1,33 @@
 package com.example.notes.adapter
 
-class NoteAdapter {
+import android.provider.ContactsContract.CommonDataKinds.Note
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.notes.R
+import com.example.notes.model.NoteModel
+
+class NoteAdapter: RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+
+    var listNote = emptyList<NoteModel>()
+
+    class NoteViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val titleView: TextView = itemView.findViewById(R.id.item_title)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false)
+        return NoteViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        holder.titleView.text = listNote[position].title
+    }
+
+    override fun getItemCount(): Int {
+        return listNote.size
+    }
+
 }
