39fd51f10aa4aef24f669a2aad39d30ec39b080b
GGennady <lessarent@gmail.com>
2024-11-01 01:59:22 +0300

    DB classes added

--- app/src/main/java/com/example/notes/db/repository/NoteRealization.kt
+++ app/src/main/java/com/example/notes/db/repository/NoteRealization.kt
@@ -1,4 +1,21 @@
 package com.example.notes.db.repository
 
-class NoteRealization {
+import androidx.lifecycle.LiveData
+import com.example.notes.db.dao.NoteDao
+import com.example.notes.model.NoteModel
+
+class NoteRealization(private val noteDao: NoteDao): NoteRepository {
+    override val allNotes: LiveData<List<NoteModel>>
+        get() = noteDao.getAll()
+
+    override suspend fun insertNote(noteModel: NoteModel, onSuccess: () -> Unit) {
+        noteDao.insert(noteModel)
+        onSuccess()
+    }
+
+    override suspend fun deleteNote(noteModel: NoteModel, onSuccess: () -> Unit) {
+        noteDao.delete(noteModel)
+        onSuccess()
+    }
+
 }
--- app/src/main/java/com/example/notes/db/repository/NoteRepository.kt
+++ app/src/main/java/com/example/notes/db/repository/NoteRepository.kt
@@ -1,5 +1,12 @@
 package com.example.notes.db.repository
 
-interface NoteRepository {
+import androidx.lifecycle.LiveData
+import com.example.notes.model.NoteModel
 
+interface NoteRepository {
+    suspend fun insertNote(noteModel: NoteModel, onSuccess:() -> Unit)
+    suspend fun deleteNote(noteModel: NoteModel, onSuccess:() -> Unit)
+
+    // a variable that stores the entire list
+    val allNotes: LiveData<List<NoteModel>>
 }
