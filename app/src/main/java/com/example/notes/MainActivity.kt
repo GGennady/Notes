4970e504ba936c3c1e5c8e37b7ee28fc2531d7ce
GGennady <lessarent@gmail.com>
2024-12-01 22:59:10 +0300

    massive changes

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.notes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import com.example.notes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding
+        get() = _binding ?: throw IllegalStateException("Binding for ActivityMain must be not null.")
+
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        APP = this
+
+        // Найдем NavHostFragment и получим NavController
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_fragment) as? NavHostFragment
+                ?: throw IllegalStateException("NavHostFragment not found!")
+        navController = navHostFragment.navController
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null // Освобождение binding
     }
 }
+
--- app/src/main/java/com/example/notes/screens/start/StartFragment.kt
+++ app/src/main/java/com/example/notes/screens/start/StartFragment.kt
@@ -5,17 +5,59 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import com.example.notes.APP
 import com.example.notes.R
+import com.example.notes.adapter.NoteAdapter
+import com.example.notes.databinding.FragmentStartBinding
 
 
 class StartFragment : Fragment() {
 
+    private var _binding: FragmentStartBinding? = null
+    private val binding
+        get() = _binding ?: throw IllegalStateException("Binding for FragmentStart must be not null.")
+
+    lateinit var recyclerView: RecyclerView
+    lateinit var adapter: NoteAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start, container, false)
+        _binding = FragmentStartBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+        val viewModel = ViewModelProvider(this)[StartViewModel::class.java]
+        viewModel.initDatabase()
+
+        recyclerView = binding.rvNotes
+        adapter = NoteAdapter()
+        recyclerView.adapter = adapter
+
+        // Что это такое?
+        // Это лямбда-функция (анонимная функция). Лямбда-функция принимает один параметр — listNotes,
+        // который представляет собой обновленный список заметок.
+        //
+        // Как это работает?
+        // Каждый раз, когда LiveData, возвращаемое viewModel.getAllNotes(), обновляет свои данные,
+        // вызывается эта лямбда-функция. Она передает обновленный список заметок в метод setList() адаптера.
+
+        viewModel.getAllNotes().observe(viewLifecycleOwner) { listNotes ->
+            listNotes.asReversed()
+            adapter.setList(listNotes)
+        }
+
+        binding.btnNext.setOnClickListener() {
+            APP.navController.navigate(R.id.action_startFragment_to_addNoteFragment)
+        }
+    }
 }
